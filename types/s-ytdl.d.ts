b3410ae2d7422793a90eff4d77ea0ea40de992a8
Alex Carrabre <alexcarrabre@gmail.com>
2025-04-19 22:46:12 -0700

    Fix: Add missing dl method to s-ytdl type declaration

--- types/s-ytdl.d.ts
+++ types/s-ytdl.d.ts
@@ -4,11 +4,14 @@ declare module 's-ytdl' {
   export function downloadStream(videoId: string, options?: any): NodeJS.ReadableStream;
   export function getVideoInfo(videoId: string): Promise<any>;
   export function getFormats(videoId: string): Promise<any[]>;
+  export function dl(url: string, quality?: string, type?: string): Promise<Buffer>;
+  
   export default {
     getInfo,
     download,
     downloadStream,
     getVideoInfo,
-    getFormats
+    getFormats,
+    dl
   };
 } 
